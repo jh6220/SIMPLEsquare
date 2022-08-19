68a2d20ce9adadbc09215913a80467d889ae66a9
Jakub Horsky <jh6220@ic.ac.uk>
2022-08-19 23:42:50 +0100

    clean folder

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ BCu[2,:] = 1 # u-velocity of 1 on the north boundary wall
 prob = sf.CFDproblem(x,y, rho, mu, BCu, BCv,dt,alpha_uv,alpha_p,CDS)
 
 # initialize arrays storing the fluid properties
-# u,v = np.zeros((prob.nx+1,prob.ny+2)),np.zeros((prob.nx+2,prob.ny+1)) # if reshaped/flattened aixs=1 (y) will remain together
-# u_star,v_star = np.zeros((prob.nx+1,prob.ny+2)),np.zeros((prob.nx+2,prob.ny+1)) # intermediate step in the calculation
-# u_vector,v_vector = u[1:-1,1:-1].flatten(),v[1:-1,1:-1].flatten()
-# p,p_prime,p_vector = np.zeros((prob.nx,prob.ny)),np.zeros((prob.nx,prob.ny)),np.zeros(prob.nx*prob.ny)
+u,v = np.zeros((prob.nx+1,prob.ny+2)),np.zeros((prob.nx+2,prob.ny+1)) # if reshaped/flattened aixs=1 (y) will remain together
+u_star,v_star = np.zeros((prob.nx+1,prob.ny+2)),np.zeros((prob.nx+2,prob.ny+1)) # intermediate step in the calculation
+u_vector,v_vector = u[1:-1,1:-1].flatten(),v[1:-1,1:-1].flatten()
+p,p_prime,p_vector = np.zeros((prob.nx,prob.ny)),np.zeros((prob.nx,prob.ny)),np.zeros(prob.nx*prob.ny)
 
 # define the iteration and convergence parameters 
 dif = 1
